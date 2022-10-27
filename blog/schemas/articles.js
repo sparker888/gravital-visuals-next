226450dee6476940809adce82c2b3bf7485a11a9
Stephen Parker <sparker888@gmail.com>
2022-10-27 19:59:45 -0400

    update gitignore to ignore node_modules in sanity

---
+++ blog/schemas/articles.js
@@ -0,0 +1,28 @@
+export default {
+  name: 'article',
+  title: 'Article',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Article Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      validation: (Rule) => Rule.required(),
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'markdown',
+    },
+  ],
+};
